8272a1e3a1bcaebc1791e8a0db0cf63716d77ca1
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-01-20 05:19:45 +0000

    Update dbchangelog.sql

--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
--- changeset liquibase:24
-INSERT INTO info_dept(id,name) VALUES ('EV','Environmental')
+-- changeset liquibase:25
+INSERT INTO info_dept(id,name) VALUES ('A25','AI25')
