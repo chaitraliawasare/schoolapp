cb9fa98b9437b514e485b91158da91e471dc368d
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-01-20 11:27:42 +0000

    Update dbchangelog.sql

--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
--- changeset liquibase:26
-INSERT INTO info_dept(id,name) VALUES ('A26','ML26')
+-- changeset liquibase:27
+INSERT INTO info_dept(id,name) VALUES ('A27','ML27')
