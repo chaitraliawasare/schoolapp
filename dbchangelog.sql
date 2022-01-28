d64df3d5cdaab93800e90d5d632870517c016df1
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-01-28 10:38:19 +0000

    Update dbchangelog.sql

--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset liquibase:31
-INSERT INTO info_dept(id,name) VALUES ('ST','Software Testing')
+INSERT INTO info_dept(id,name) VALUES ('Micro','Microprocessor')
