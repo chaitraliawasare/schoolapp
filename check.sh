b995b4ac84623e5586f5922c8dcca8e80ec2cca5
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-02-10 08:47:22 +0000

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#cd /var/jenkins/workspace/Build_Web_App_1/College-ERP-master
+#pip3 install -r requirements.txt
+
+a= pgrep -f '/usr/bin/python3 manage.py runserver 0:8000'
+echo $a
+
+if [ $a > 0 ]
+then
+   echo "Running"
+else
+   echo "Not Running"
+fi
