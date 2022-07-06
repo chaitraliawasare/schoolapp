3be0557ef6555f1b8c7896e136b906545f257674
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-07-06 11:20:27 +0000

    Update dbchangelog.sql

--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
--- changeset liquibase:35
-INSERT INTO info_dept(id,name) VALUES ('Mic3','Microprocessor3')
+-- changeset liquibase:36
+INSERT INTO info_dept(id,name) VALUES ('Mic','Microprocessor')
